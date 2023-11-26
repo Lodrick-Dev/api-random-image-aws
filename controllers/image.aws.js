d2270d655c5a458f30457950560eadd9dcc17159
Lodrick <dev.frenchlod@gmail.com>
2023-11-26 19:52:03 +0100

    ✅ small update

--- controllers/image.aws.js
+++ controllers/image.aws.js
@@ -219,7 +219,10 @@ module.exports.reactionUserOnImageOrPullReaction = async (req, res) => {
 module.exports.getOneImageMongo = async (req, res) => {
   try {
     //le size : 1 pour dire je veux seul document
-    const imageRandom = await ImageModel.aggregate([{ $sample: { size: 1 } }]);
+    //dans crochet pour obtenir l'objet et non le tableau
+    const [imageRandom] = await ImageModel.aggregate([
+      { $sample: { size: 1 } },
+    ]);
     return res.status(200).send(imageRandom);
   } catch (error) {
     console.log("Error to getOneImageMongo");
