dcaa7b0975a55b72be86d72c176b7175e91840fa
Lodrick <dev.frenchlod@gmail.com>
2023-12-07 15:20:45 +0100

    ✨ intro new methode to comment

--- controllers/image.aws.js
+++ controllers/image.aws.js
@@ -161,77 +161,8 @@ module.exports.reactionUserOnImageSaveInMongo = async (req, res, next) => {
   }
 };
 
-//mem code mais vérifie si l'utilisateur retire sa reaction not use
-// module.exports.reactionUserOnImageOrPullReaction = async (req, res) => {
-//   const { id, emailuser, react } = req.body;
-
-//   try {
-//     // Trouver l'image par ID
-//     const image = await ImageModel.findById(id);
-
-//     // Vérifier si l'image existe
-//     if (!image) {
-//       return res
-//         .status(200)
-//         .json({ message: "Erreur : Identification de l'image impossible" });
-//     }
-
-//     // Vérifier si l'utilisateur a déjà réagi
-//     const userAlreadyReacted = image.reactionsusers.some(
-//       (reaction) => reaction.emailuser === emailuser
-//     );
-
-//     if (userAlreadyReacted) {
-//       // L'utilisateur a déjà réagi, mise à jour de sa réaction
-//       try {
-//         // Vérifier si react est null avant de mettre à jour
-//         if (react !== null) {
-//           await ImageModel.updateOne(
-//             { _id: image._id, "reactionsusers.emailuser": emailuser },
-//             { $set: { "reactionsusers.$.reaction": react } }
-//           );
-//         } else {
-//           // Si react est null, retirer l'objet contenant l'e-mail de l'utilisateur
-//           await ImageModel.updateOne(
-//             { _id: image._id },
-//             { $pull: { reactionsusers: { emailuser } } }
-//           );
-//         }
-//       } catch (error) {
-//         console.log(
-//           "Erreur lors de la mise à jour de la réaction de l'utilisateur : " +
-//             error
-//         );
-//       }
-//     } else {
-//       // L'utilisateur n'a pas encore réagi, ajouter sa réaction si react n'est pas null
-//       if (react !== null) {
-//         try {
-//           await ImageModel.updateOne(
-//             { _id: image._id },
-//             { $push: { reactionsusers: { emailuser, reaction: react } } }
-//           );
-//         } catch (error) {
-//           console.log(
-//             "Erreur si l'utilisateur n'a pas encore réagi : " + error
-//           );
-//         }
-//       }
-//     }
-
-//     return res
-//       .status(200)
-//       .json({ message: "Réaction de l'utilisateur mise à jour avec succès" });
-//   } catch (error) {
-//     console.log(
-//       "Erreur lors de la gestion de la réaction de l'utilisateur : " + error
-//     );
-//     return res.status(500).json({
-//       message:
-//         "Erreur serveur lors de la gestion de la réaction de l'utilisateur",
-//     });
-//   }
-// };
+//put comment - token
+module.exports.commentImage = async (req, res, next) => {};
 
 //one random image from mongoDB
 module.exports.getOneImageMongo = async (req, res) => {
